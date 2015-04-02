e171dcf457cb16b2ed5213329f1714c9e02e5bcc
Linus Oleander <linus@oleander.nu>
2015-04-02 21:50:30 +0200

    Fix Track spec

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -1,12 +1,8 @@
 describe Track do
   context "fp" do
     it "should work" do
-      path = Rails.root.join("spec/fixtures/fingerprint.json")
-      data = File.read(path)
-      json = JSON.parse(data)
-
-      fp = Fingerprint::Inflate.new(json.first.fetch("code")).inflate
-
+      data = json_fixture("finger-input.json")
+      fp = Fingerprint::Inflate.new(data.first[:code]).inflate
       Track.fp(fp)
     end
   end
